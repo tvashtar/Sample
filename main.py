1f251631c433bc705360e0ef36fa54a7d5b67da0
Conor Laver <conorlaver@gmail.com>
2022-03-20 16:04:29 -0400

    seeing if this removes idea

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print('testing out breakpoints')
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
 
